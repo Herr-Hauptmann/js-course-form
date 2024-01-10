09fcecfb3d752b6e293eb93e69af8ec26f470810
Tarik Horozovic <tarik.horoz@gmail.com>
2024-01-10 19:44:21 +0100

    add data

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+//isprazniti prvi select
+//u njega dodati sve iz niza
+//dodati listener na change
+//u odnosu na odabir isprazniti drugi select i popuniti ga ILI ga sakriti a prikazati input
---
+++ public/js/data.js
@@ -0,0 +1,60 @@
+const petTypes = [
+    {
+        id : 1,
+        name: "Dog"
+    },
+    {
+        id : 2,
+        name: "Cat"
+    },
+    {
+        id : 3,
+        name: "Other"
+    }
+];
+
+const dogBreeds = [
+    {
+        id: 1,
+        name: "Labrador"
+    },
+    {
+        id: 2,
+        name: "Buldog"
+    },
+    {
+        id: 3,
+        name: "Pudlica"
+    },
+    {
+        id: 4,
+        name: "Njemacki ovacr"
+    },
+    {
+        id: 5,
+        name: "Zlatni retriver"
+    },
+];
+
+const catBreeds = [
+    {
+        id: 1,
+        name: "Perzijska macka"
+    },
+    {
+        id: 2,
+        name: "Americka kratkodlaka macka"
+    },
+    {
+        id: 3,
+        name: "Sfinks macka"
+    },
+    {
+        id: 4,
+        name: "Ragdoll"
+    },
+    {
+        id: 5,
+        name: "Bengalska macka"
+    },
+]
