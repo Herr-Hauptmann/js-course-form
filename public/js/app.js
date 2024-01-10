684ef3aa3269f96005fc082ea9b1010c11acd4c2
Tarik Horozovic <tarik.horoz@gmail.com>
2024-01-10 19:57:10 +0100

    remove elements

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,7 @@
 //u njega dodati sve iz niza
 //dodati listener na change
 //u odnosu na odabir isprazniti drugi select i popuniti ga ILI ga sakriti a prikazati input
+let petSelect = document.getElementById("petType");
+Array.from(petSelect.children).forEach(element => {
+   element.remove();
+});
