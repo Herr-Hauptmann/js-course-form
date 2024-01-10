6c0f3c89b9e37eea464759742c8b0b371494bd35
Tarik Horozovic <tarik.horoz@gmail.com>
2024-01-10 20:05:23 +0100

    add children to select

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,13 @@
 //dodati listener na change
 //u odnosu na odabir isprazniti drugi select i popuniti ga ILI ga sakriti a prikazati input
 let petSelect = document.getElementById("petType");
-Array.from(petSelect.children).forEach(element => {
-   element.remove();
+Array.from(petSelect.children).forEach((element) => {
+  element.remove();
+});
+
+petTypes.forEach((el) => {
+  const listItem = document.createElement("option");
+  listItem.value = el.id;
+  listItem.innerHTML = el.name;
+  petSelect.appendChild(listItem);
 });
